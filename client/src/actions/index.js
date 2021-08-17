ba5ebde90070007d70980764b0082473fdc6a41c
unknown <avancini16@gmail.com>
2021-08-17 18:39:11 -0300

    actions updates

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 export function getPokemons() {
     return function (dispatch) {
-      return fetch('http://localhost:3001/morePoke')
+      return fetch('http://localhost:3001/pokemons')
         .then(response => response.json())
         .then(json => {
           dispatch({ type: "GET_POKEMONS", payload: json });
